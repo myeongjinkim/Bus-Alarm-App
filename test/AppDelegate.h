05a0c0c829f52ffb4dcc3cea173158479b69890b
Kim Hyun Gyu <hyungyukim@hyunui-macbook-air.local>
2016-06-25 15:09:22 +0900

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Hyun Gyu Kim on 2016. 6. 25..
+//  Copyright © 2016년 Hyun Gyu Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
